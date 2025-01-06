0ed3b3ce8be4d15428fe7fe5cc4d082ef383043b
AndreyBobb <regov.maks@mail.ru>
2025-01-06 21:19:57 +0300

    Exercises

---
+++ src/test/java/selenide/TestHover.java
@@ -0,0 +1,32 @@
+package selenide;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestHover {
+    @Test
+    void shouldOpenEnterprises(){
+        open("https://github.com/");
+        $("div.HeaderMenu nav ul.d-lg-flex li").sibling(0).hover();
+
+        $("div.HeaderMenu-column div.border-bottom #solutions-by-company-size-heading")
+                .closest("div").$(byText("Enterprises")).click();
+
+        $("div h1#hero-section-brand-heading").sibling(0)
+                .shouldHave(text("To build, scale, and deliver secure software."));
+    }
+
+    @Test
+    void switchRectangles(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("div#column-a")).clickAndHold()
+                .moveToElement($("div#column-b")).release().perform();
+        $("div#columns>div").shouldHave(text("B"));
+
+        sleep(3000);
+    }
+}
