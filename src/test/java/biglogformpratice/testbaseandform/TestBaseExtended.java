65006ce5bfb02d38bb8d99ddd3548fb3371b4496
AndreyBobb <regov.mak@mail.ru>
2025-02-03 19:44:06 +0300

    Added attaches - video

--- src/test/java/biglogformpratice/testbaseandform/TestBaseExtended.java
+++ src/test/java/biglogformpratice/testbaseandform/TestBaseExtended.java
@@ -3,7 +3,9 @@ package biglogformpratice.testbaseandform;
 import biglogformpratice.testPackage.pages.RegistrationPage;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -34,6 +36,14 @@ public class TestBaseExtended {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+//        Attach.addVideo();
     }
 
 }
